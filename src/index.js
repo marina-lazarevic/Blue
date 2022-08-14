691c0a8c055e40fee529c0447638fb0d0110c2d1
Marina <lazarevic.ui@gmail.com>
2022-08-14 05:41:34 +0200

    responsive about section

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import './styles/main.scss';
 
 import branding_img from './assets/branding.png';
+import about_img from './assets/about.png';
 import goal_img from './assets/goal.png';
 
 document.querySelectorAll('.logo').forEach(logo_img => {
     logo_img.src = branding_img;
 });
+document.querySelector('#about-img').src = about_img;
 document.querySelector('#goal-img').src = goal_img;
